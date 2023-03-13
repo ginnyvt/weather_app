da287952b2476d0fb57e51b78eda83fba177f450
ginnyvt <ginny@powerkernel.com>
2023-03-13 23:03:12 +0200

    Removed extra comment codes

--- src/components/ForecastWeather.tsx
+++ src/components/ForecastWeather.tsx
@@ -33,51 +33,6 @@ const ForecastWeather: React.FC<ForecastWeatherProps> = ({ forecastWeather }) =>
 							</IonCard>
 						</IonCol>
 					))}
-
-					{/* <IonCol size="12" size-sm="2.4">
-						<IonCard>
-							<IonCardHeader>
-								<IonCardTitle>Helsinki</IonCardTitle>
-								<IonCardSubtitle>Clear sky</IonCardSubtitle>
-							</IonCardHeader>
-							<IonCardContent>
-								Here's a small text description for the card content. Nothing more, nothing less.
-							</IonCardContent>
-						</IonCard>
-					</IonCol>
-					<IonCol size="12" size-sm="2.4">
-						<IonCard>
-							<IonCardHeader>
-								<IonCardTitle>Helsinki</IonCardTitle>
-								<IonCardSubtitle>Clear sky</IonCardSubtitle>
-							</IonCardHeader>
-							<IonCardContent>
-								Here's a small text description for the card content. Nothing more, nothing less.
-							</IonCardContent>
-						</IonCard>
-					</IonCol>
-					<IonCol size="12" size-sm="2.4">
-						<IonCard>
-							<IonCardHeader>
-								<IonCardTitle>Helsinki</IonCardTitle>
-								<IonCardSubtitle>Clear sky</IonCardSubtitle>
-							</IonCardHeader>
-							<IonCardContent>
-								Here's a small text description for the card content. Nothing more, nothing less.
-							</IonCardContent>
-						</IonCard>
-					</IonCol>
-					<IonCol size="12" size-sm="2.4">
-						<IonCard>
-							<IonCardHeader>
-								<IonCardTitle>Helsinki</IonCardTitle>
-								<IonCardSubtitle>Clear sky</IonCardSubtitle>
-							</IonCardHeader>
-							<IonCardContent>
-								Here's a small text description for the card content. Nothing more, nothing less.
-							</IonCardContent>
-						</IonCard>
-					</IonCol> */}
 				</IonRow>
 			</IonGrid>
 		</div>

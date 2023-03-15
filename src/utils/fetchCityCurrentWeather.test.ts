43ccac3c3e6e10b93807704c82a2e3d8b32efb46
ginnyvt <ginny@powerkernel.com>
2023-03-15 20:33:56 +0200

    Updated mock test function for fetch API

--- src/utils/fetchCityCurrentWeather.test.ts
+++ src/utils/fetchCityCurrentWeather.test.ts
@@ -1,11 +1,21 @@
 import { cities } from "../assets/cityData";
 import { fetchCityCurrentWeather } from "./fetchCityCurrentWeather";
 
-const axios = require("axios");
+function mockFetch() {
+	return jest.spyOn(window, "fetch").mockResolvedValue(
+		new Response(
+			JSON.stringify({
+				dt: 1618319060,
+				main: { temp: 12.34 },
+				weather: [{ description: "clear sky" }],
+				wind: { speed: 3.6 },
+				rain: null,
+			})
+		)
+	);
+}
 
-// Mock the axios module
-jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+beforeEach(() => mockFetch());
 
 describe("fetchCityCurrentWeather", () => {
 	afterEach(() => {
@@ -15,42 +25,14 @@ describe("fetchCityCurrentWeather", () => {
 	it("fetches the current weather for a given city", async () => {
 		const city = cities[0];
 
-		const mockResponse = {
+		const result = await fetchCityCurrentWeather(city);
+
+		expect(result).toEqual({
 			dt: 1618319060,
 			main: { temp: 12.34 },
 			weather: [{ description: "clear sky" }],
 			wind: { speed: 3.6 },
 			rain: null,
-		};
-
-		mockedAxios.get.mockResolvedValue({ data: mockResponse });
-
-		const result = await fetchCityCurrentWeather(city);
-
-		expect(mockedAxios.get).toHaveBeenCalledWith(`${process.env.REACT_APP_OPEN_WEATHER_URL}/weather`, {
-			params: {
-				appid: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
-				lat: city.coord.lat,
-				lon: city.coord.lon,
-				units: "metric",
-			},
 		});
-		expect(result).toEqual({
-			dt: mockResponse.dt,
-			main: mockResponse.main,
-			weather: mockResponse.weather,
-			wind: mockResponse.wind,
-			rain: mockResponse.rain,
-		});
-	});
-
-	it("returns false when the request fails", async () => {
-		const city = cities[0];
-
-		mockedAxios.get.mockRejectedValue(new Error("Request failed."));
-
-		const result = await fetchCityCurrentWeather(city);
-
-		expect(result).toBe(false);
 	});
 });
--- src/utils/fetchCityForecastWeather.test.ts
+++ src/utils/fetchCityForecastWeather.test.ts
@@ -1,11 +1,34 @@
 import { cities } from "../assets/cityData";
 import { fetchCityForecastWeather } from "./fetchCityForecastWeather";
 
-// Mock the axios module
-const axios = require("axios");
+//global.fetch = jest.fn(async () => Promise.resolve({ json: async () => Promise.resolve({ id: 0 }) })) as jest.Mock;
 
-jest.mock("axios");
-const mockedAxios = axios as jest.Mocked<typeof axios>;
+function mockFetch() {
+	return jest.spyOn(window, "fetch").mockResolvedValue(
+		new Response(
+			JSON.stringify({
+				list: [
+					{
+						dt: 1618319060,
+						main: { temp: 12.34 },
+						weather: [{ description: "clear sky" }],
+						wind: { speed: 3.6 },
+						rain: null,
+					},
+					{
+						dt: 1618319061,
+						main: { temp: 12.34 },
+						weather: [{ description: "broken clouds" }],
+						wind: { speed: 3.6 },
+						rain: null,
+					},
+				],
+			})
+		)
+	);
+}
+
+beforeEach(() => mockFetch());
 
 describe("fetchCityForecastWeather", () => {
 	afterEach(() => {
@@ -14,8 +37,8 @@ describe("fetchCityForecastWeather", () => {
 
 	it("fetches the forecast weather for a given city", async () => {
 		const city = cities[0];
-
-		const mockResponse = {
+		const result = await fetchCityForecastWeather(city);
+		expect(result).toEqual({
 			list: [
 				{
 					dt: 1618319060,
@@ -32,31 +55,6 @@ describe("fetchCityForecastWeather", () => {
 					rain: null,
 				},
 			],
-		};
-
-		mockedAxios.get.mockResolvedValue({ data: mockResponse });
-
-		const result = await fetchCityForecastWeather(city);
-
-		expect(mockedAxios.get).toHaveBeenCalledWith(`${process.env.REACT_APP_OPEN_WEATHER_URL}/forecast`, {
-			params: {
-				appid: process.env.REACT_APP_OPEN_WEATHER_API_KEY,
-				lat: city.coord.lat,
-				lon: city.coord.lon,
-				units: "metric",
-				cnt: 5,
-			},
 		});
-		expect(result).toEqual(mockResponse);
-	});
-
-	it("returns false when the request fails", async () => {
-		const city = cities[0];
-
-		mockedAxios.get.mockRejectedValue(new Error("Request failed"));
-
-		const result = await fetchCityForecastWeather(city);
-
-		expect(result).toBe(false);
 	});
 });

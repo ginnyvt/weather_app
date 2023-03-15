182cc5e7dd33b301213ca669c53fd53609c43305
ginnyvt <ginny@powerkernel.com>
2023-03-15 20:32:32 +0200

    Modified fetch functions using fetch API

--- src/utils/fetchCityCurrentWeather.ts
+++ src/utils/fetchCityCurrentWeather.ts
@@ -1,20 +1,18 @@
-import axios from "axios";
 import { City, WeatherData } from "../types/dataTypes";
 
 const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 const baseUrl = process.env.REACT_APP_OPEN_WEATHER_URL;
 
 export const fetchCityCurrentWeather = async (city: City): Promise<WeatherData | false> => {
+	const queryParams = new URLSearchParams({
+		appid: apiKey!,
+		lat: city.coord.lat.toString(),
+		lon: city.coord.lon.toString(),
+		units: "metric",
+	});
 	try {
-		const { data } = await axios({
-			url: `${baseUrl}/weather`,
-			params: {
-				appid: apiKey,
-				lat: city.coord.lat,
-				lon: city.coord.lon,
-				units: "metric",
-			},
-		});
+		const response = await fetch(`${baseUrl}/weather/?${queryParams}`);
+		const data = await response.json();
 		const currentWeather: WeatherData = {
 			dt: data.dt,
 			main: data.main,
--- src/utils/fetchCityForecastWeather.ts
+++ src/utils/fetchCityForecastWeather.ts
@@ -1,23 +1,20 @@
-import axios from "axios";
 import { City, ForecastWeatherData } from "../types/dataTypes";
 
 const apiKey = process.env.REACT_APP_OPEN_WEATHER_API_KEY;
 const baseUrl = process.env.REACT_APP_OPEN_WEATHER_URL;
 
 export const fetchCityForecastWeather = async (city: City): Promise<ForecastWeatherData | false> => {
+	const queryParams = new URLSearchParams({
+		appid: apiKey!,
+		lat: city.coord.lat.toString(),
+		lon: city.coord.lon.toString(),
+		units: "metric",
+		cnt: "5",
+	});
 	try {
-		const response = await axios({
-			url: `${baseUrl}/forecast`,
-			params: {
-				appid: apiKey,
-				lat: city.coord.lat,
-				lon: city.coord.lon,
-				units: "metric",
-				cnt: 5,
-			},
-		});
-
-		return response.data;
+		const response = await fetch(`${baseUrl}/forecast/?${queryParams}`);
+		const data: ForecastWeatherData = await response.json();
+		return data;
 	} catch (error) {
 		return false;
 	}

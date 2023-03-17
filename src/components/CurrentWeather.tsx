d64d9bf465c0a316e77765e4c59ed7ea32e0cc1b
ginnyvt <ginny@powerkernel.com>
2023-03-17 11:48:41 +0200

    Modified component structure and styling

--- src/components/CurrentWeather.tsx
+++ src/components/CurrentWeather.tsx
@@ -10,50 +10,50 @@ interface CurrentWeatherProps {
 }
 
 const CurrentWeather: React.FC<CurrentWeatherProps> = ({ currentWeather, cityName }) => {
+	const iconUrl = process.env.REACT_APP_OPEN_WEATHER_ICON_URL;
+	let content;
 	if (currentWeather === false) {
-		return (
-			<div className="container">
-				<p>No data</p>
-			</div>
+		content = (
+			<IonCardContent>
+				<p>No data available</p>
+			</IonCardContent>
+		);
+	} else {
+		const dt = extractDateTime(currentWeather.dt);
+		content = (
+			<IonCardContent>
+				<div className={styles["card-top"]}>
+					<div className={styles["card-top-left"]}>
+						<h2>{cityName}</h2>
+						<p>{currentWeather.weather[0].description}</p>
+					</div>
+					<div className={styles["card-top-right"]}>
+						<div>
+							<img src={`${iconUrl}/${currentWeather!.weather[0].icon}@2x.png`} alt="Weather icon" />
+						</div>
+						<h2>{Math.round(currentWeather.main.temp)} °C</h2>
+					</div>
+				</div>
+				<div className={styles["card-bottom"]}>
+					<div className={styles["card-bottom-left"]}>
+						<h3>{dt.date}</h3>
+						<p>{dt.time}</p>
+					</div>
+					<div className={styles["card-bottom-right"]}>
+						<p>Wind: {currentWeather.wind.speed} m/s</p>
+						<p>Humidity: {currentWeather.main.humidity}%</p>
+						<p>Precipitation (3 h): {currentWeather.rain ? currentWeather.rain["3h"] : 0} mm</p>
+					</div>
+				</div>
+			</IonCardContent>
 		);
 	}
 
-	const dt = extractDateTime(currentWeather.dt);
 	return (
 		<IonGrid>
 			<IonRow className={styles["row"]}>
 				<IonCol size="12" className={styles["row"]}>
-					<IonCard className={styles["card"]}>
-						<IonCardContent>
-							<div className={styles["card-top"]}>
-								<div className={styles["card-top-left"]}>
-									<h2>{cityName}</h2>
-									<p>{currentWeather.weather[0].description}</p>
-								</div>
-								<div className={styles["card-top-right"]}>
-									<div>
-										<img src={`https://openweathermap.org/img/wn/${currentWeather!.weather[0].icon}@2x.png`} alt="Weather icon" />
-									</div>
-									<h2>{currentWeather.main.temp}°C</h2>
-								</div>
-							</div>
-							<div className={styles["card-bottom"]}>
-								<div className={styles["card-bottom-left"]}>
-									<p>{dt.date}</p>
-									<p>{dt.time}</p>
-								</div>
-								<div className={styles["card-bottom-right"]}>
-									<p>Wind: {currentWeather.wind.speed} m/s</p>
-									<p>Humidity: {currentWeather.main.humidity}%</p>
-									{currentWeather.rain ? (
-										<p>Precipitation: {currentWeather.rain["3h"]} mm</p>
-									) : (
-										<p>No precipitation in the next 3 hours</p>
-									)}
-								</div>
-							</div>
-						</IonCardContent>
-					</IonCard>
+					<IonCard className={styles["card"]}>{content}</IonCard>
 				</IonCol>
 			</IonRow>
 		</IonGrid>
